208a6ef109e8d8a4771cdd0d3764dcff5278c16a
kkpedrok <pedro.k@outlook.com>
2019-02-07 17:36:12 -0200

    newV

--- analisador.py
+++ analisador.py
@@ -2,11 +2,13 @@
 # -*- coding: latin-1 -*-
 import io
 import time
+import datetime
 
-ARQUIVO_TRANSPORTADORA_CSV = 'abrangencia.csv'
-ARQUIVO_CEP_CSV = 'ceps_para_analise.csv'
-ARQUIO_RESULTADO = 'cep_por_transportadora.txt'
-ARQUIVO_LOG = 'cep_por_transportadora_log.txt'
+
+ARQUIVO_TRANSPORTADORA_CSV = 'input-abrangencia.csv'
+ARQUIVO_CEP_CSV = 'input-cep.csv'
+ARQUIO_RESULTADO = 'output/analise_' + str(datetime.datetime.now().strftime('%Y-%m-%d_%Hh%Mmin')) + '.csv'
+ARQUIVO_LOG = 'analise_log.txt'
 
 
 def _log(texto):
@@ -17,14 +19,21 @@ def _log(texto):
 
 
 def _gerar_arquivo_de_ceps(cep_por_transportadora):
+    
     retorno = open(ARQUIO_RESULTADO, "a")
     retorno.write('{}\n'.format(cep_por_transportadora))
     retorno.close()
 
 
 def _comparar_cep_transportadora(ceps):
-    textfile = io.open(ARQUIVO_TRANSPORTADORA_CSV, 'rt', newline='', encoding='utf-8')
+    textfile = io.open(ARQUIVO_TRANSPORTADORA_CSV, 'rt',
+                       newline='', encoding='latin_1')
     linhas_transportadora = textfile.readlines()
+
+
+    
+    
+
     for indice_cep in range(1, len(ceps)):
         cep = ceps[indice_cep]
         cep_por_transportadora = cep.zfill(8)
@@ -32,32 +41,35 @@ def _comparar_cep_transportadora(ceps):
             try:
                 cep_transportadora = linhas_transportadora[indice_linha_transportadora].split(';')
                 tokens = [token for token in cep_transportadora]
-                metodo_envio = tokens[0]
-                regiao = tokens[1]
-                uf = tokens[2]
-                cidade = tokens[3]
-                praca = tokens[4]
-                cep_inicial = tokens[5]
-                cep_final = tokens[6]
-                prazo = tokens[7]
-                custo = tokens[8]
+                metodo_envio = tokens[1]
+                # regiao = tokens[1]
+                # uf = tokens[2]
+                # cidade = tokens[3]
+                tarifa = tokens[6]
+                cep_inicial = tokens[4]
+                cep_final = tokens[5]
+                # prazo = tokens[7]
+                # custo = tokens[8]
                 if int(cep_inicial) <= int(ceps[indice_cep]) <= int(
                         cep_final) and metodo_envio not in cep_por_transportadora:
                     cep_por_transportadora += ("; {}".format(metodo_envio)
-                                               + "; {}".format(regiao)
-                                               + "; {}".format(uf)
-                                               + "; {}".format(cidade)
-                                               + "; {}".format(praca)
-                                               + "; {}".format(prazo)
-                                               + "; {}".format(custo)
+                                            #    + "; {}".format(regiao)
+                                            #    + "; {}".format(uf)
+                                            #    + "; {}".format(cidade)
+                                               + "; {}".format(tarifa)
+                                            #    + "; {}".format(prazo)
+                                            #    + "; {}".format(custo)
                                                )
             except Exception as e:
-                mensagem_erro = "Erro na linha {}; {}".format(indice_linha_transportadora, e)
+                mensagem_erro = "Erro na linha {}; {}".format(
+                    indice_linha_transportadora, e)
                 _log(mensagem_erro)
-        _gerar_arquivo_de_ceps(cep_por_transportadora.replace('\n', '').replace('\r', ''))
+        _gerar_arquivo_de_ceps(
+            cep_por_transportadora.replace('\n', '').replace('\r', ''))
 
 
 def gerar():
+    
     start_time = time.time()
     _log('Tag Livros - Ouro & Fino - Iniciando verificcao de CEP por transportadora ')
     textfile = io.open(ARQUIVO_CEP_CSV, 'rt', newline='', encoding='utf-8')
@@ -69,5 +81,6 @@ def gerar():
     _log("Tag Livros - Ouro & Fino - Finalizado")
     _log("timer:{}min{}seg".format(minutos, segundos))
     textfile.close()
+
 
 print(gerar())
--- teste.py
+++
@@ -1,8 +0,0 @@
-import os
-path = os.getcwd()
-files = os.listdir(path)
-print(files)
-
-files_xls = [f for f in files if f[-3:] == 'xls']
-
-print (files_xls)
