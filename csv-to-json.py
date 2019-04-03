59aad63c2851696db8720cf5d268d3dd6841ea66
kkpedrok <pedro.k@outlook.com>
2019-04-03 17:36:44 -0300

    simulação azul

--- csv-to-json.py
+++ csv-to-json.py
@@ -4,11 +4,10 @@ import json
 csvfile = open('input-abrangencia.csv', 'r', encoding='cp860')
 jsonfile = open('file.json', 'w')
 
-fieldnames = ("Transportadora","Metodo de Envio","UF","Cidade", 'CEP Inicial', 'CEP Final', 'Tarifa', 'Prazo de Entrega')
-reader = csv.DictReader( csvfile, fieldnames,  delimiter=';')
+fieldnames = ("Transportadora", "Metodo de Envio", "UF", "Cidade",
+              'CEP Inicial', 'CEP Final', 'Tarifa', 'Prazo de Entrega')
+reader = csv.DictReader(csvfile, fieldnames,  delimiter=';')
 for row in reader:
-    print (row)
+    print(row)
     json.dump(row, jsonfile, ensure_ascii=False)
     jsonfile.write('\n')
-
-
